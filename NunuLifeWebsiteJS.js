770666a92b8f387ed6ae0480c5fde7ef31dcfb9b
eyevent <eyevent@USSEAGirls05.ey.net>
2018-07-25 14:02:14 -0700

    adding all files

---
+++ NunuLifeWebsiteJS.js
@@ -0,0 +1,10 @@
+function pencil() {
+  document.getElementById('pencil').innerHTML = "Cause it's pointless!!!";
+  document.getElementById('pencil').style.fontSize='35px';
+}
+function saltMan() {
+  document.getElementById('saltMan').style.display='block';
+}
+function No() {
+  document.getElementById('No').style.display='block';
+}
